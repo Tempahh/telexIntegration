80e031eb639aee2e8cd78f5a1eef758be4b710f3
Prosper <plance991@gmail.com>
2025-02-22 12:13:09 +0100

    [fix]: add CORS support to the server and update package dependencies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,7 @@ let messageCount = 0;
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.get("/", (req, res) => {
